32844d8e75f4ca201bfc194ad9bb19b6b40ee588
Denis Anikin <ad@xfenix.ru>
2018-08-22 00:14:30 +0300

    Models initial wip

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Language(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    slug = models.SlugField(max_length=255, verbose_name=_('Alias'))
+
+    class Meta:
+        ordering = ['-pk',]
+        verbose_name = _('Programming language')
+        verbose_name_plural = _('Programming languages')
+
+
+class PackageRequest(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    language = models.ForeignKey('Language', verbose_name=_('Language'), on_delete=models.PROTECT)
